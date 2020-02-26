b761348d7716341bce3606b2d2548c9d75e8a058
xiaoma513 <1104672014@qq.com>
2020-02-26 09:38:50 +0800

    添加文件目录

---
+++ src/main/java/com/online/edu/xiaoma_edu/XiaomaEduApplication.java
@@ -0,0 +1,13 @@
+package com.online.edu.xiaoma_edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaomaEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaomaEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/online/edu/xiaoma_edu/XiaomaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.edu.xiaoma_edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaomaEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
